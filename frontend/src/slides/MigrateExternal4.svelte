bfc9bd12fd292fb51244e28069239cec687bf908
zoedesimone <56321398+zoedesimone@users.noreply.github.com>
2023-05-01 09:02:56 -0400

    updated external mig 4 to old bar chart

--- frontend/src/slides/MigrateExternal4.svelte
+++ frontend/src/slides/MigrateExternal4.svelte
@@ -1,7 +1,6 @@
 <script>
     import Slide from '../components/Slide.svelte';
-    import Bar from '../components/Bar.svelte';
-    import Scatterplot from "../components/Scatterplot.svelte";
+    import Bar from '../components/BarOld.svelte';
 
     import { onMount } from 'svelte';
     export let scrollUp;
