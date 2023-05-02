71b857f92980486b5b6bbba34f545d4860f05c91
Niklas Mannhardt <niklas.manhardt@gmail.com>
2023-05-02 13:54:22 -0400

    script

---
+++ frontend/nojekyll-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ORIGINAL_BRANCH=$(git branch | grep '\* ' | sed 's/\* //');
+
+# git config user.email niklas.manhardt@gmail.com
+
+# switch to gh-pages branch
+git stash
+git checkout gh-pages
+
+# create and push .nojekyll
+# touch .nojeykyll
+# git add .nojekyll
+# git commit -m "auto-commit add .nojekyll"
+# git push origin gh-pages
+
+# # switch to original branch
+# git checkout $ORIGINAL_BRANCH
+# git stash pop
