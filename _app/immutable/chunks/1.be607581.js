9fb3d51a3ba0361b48c8a0ccc0325445d895b97c
Niklas Mannhardt <niklas.manhardt@gmail.com>
2023-05-14 20:59:18 -0400

    Updates

--- _app/immutable/chunks/1.be607581.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.d6036eb8.js";export{t as component};
--- _app/immutable/chunks/2.49d7730d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.22b2b63f.js";export{t as component};
